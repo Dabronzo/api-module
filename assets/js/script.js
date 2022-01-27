dfc241845442e2b5389009bb1b239505af029553
Dabronzo <ayrtonterceiro@gmail.com>
2022-01-27 18:02:28 +0000

    Wired up the post to retrive an errors object

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,13 @@ const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"))
 //listener for the button thata has the id of status
 //will call a function getStatus()
 //is always a good idea pass the event in these type of functions "getStatus()"
-document.getElementById("status").addEventListener("click", e => getStatus(e))
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+//getting the listener for the post method.
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 
-//using the async to work with promises
+//using the async to work with promises on the get method
 
 async function getStatus(e){
     //defining a string concatenating api_key and api_url
@@ -31,6 +34,41 @@ async function getStatus(e){
     } else{
         throw new Error(data.error);
     }
+
+}
+
+
+async function postForm(e){
+    //then we create a form varible as a javascript object FormData
+    const form = new FormData(document.getElementById("checksform"));
+
+    //here we take the response from the fetch, used as exeple on the api documentation
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+    //now we use await for the json convertion
+    const data = await response.json();
+
+    if (response.ok){
+        console.log(data);
+
+    } else{
+        throw new Error(data.error);
+    }
+
+
+}
+
+function displayResusts(data){
+    let resultHeading = "JSHint Results";
+    
+
+    document.getElementById("resultsModalTitle").innerText = resultHeading;
+    document.getElementById("results-content").innerHTML = `<p>Your key is valid until: ${data.expiry}</p>`;
 
 }
 
